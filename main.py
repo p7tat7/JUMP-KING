0d03169ba9cbac802d5d535d89f22564a1a365be
unknown <chiangtinhang3@gmail.com>
2022-05-08 12:42:24 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+class MainCharacter():
+    def __init__(self, x, y):
+        self.x_coor = x
+        self.y_coor = y
+
+def main():
+    # Create Character
+    king = MainCharacter(1, 1)
+    # Create Environment
+
+if __name__ == '__main__':
+    main()
+
